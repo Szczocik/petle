7b23b4e8cc7cf5b8ad0cc4ee73a7472941b1a4c4
popam <popa.mariusz@gmail.com>
2021-08-17 11:20:39 +0200

    zadanie dorosli, dzieci, wino

--- main.py
+++ main.py
@@ -60,3 +60,117 @@ if count:
     print("Suma: ", sum, "średnia: ", sum/count)
 else:
     print("Suma: ", 0, "średnia: ", 0)
+
+counter = 0
+product = 1
+while counter < 10:
+    counter += 1
+    print("obliczam {}".format(counter))
+    product *= counter
+    if product > 1000:
+        break
+    print("{}: {}".format(counter, product))
+
+
+wine_count = 0
+children_count = 0
+adult_count = 0
+passenger_count = 0
+while passenger_count < 20:
+    print("podaj wiek pasażera")
+    age = int(input())
+    if age == 0:
+        break
+    elif age < 18:
+        children_count += 1
+        passenger_count += 1
+        continue
+    print("podaj liczbę lampek wina")
+    wine_current = int(input())
+    if wine_current < 0 or wine_current > 3:
+        print("nieprawidłowa liczba lampek wina")
+        continue
+    wine_count += wine_current
+    adult_count += 1
+    passenger_count += 1
+print("Dorosłych: {}, Dzieci: {}, lampek wina: {}, Przychód: {}".format(
+    adult_count,
+    children_count,
+    wine_count,
+    adult_count*400 + children_count*200 + wine_count*10
+))
+
+
+
+for i in range(6):
+    print("Pętla 1 {}".format(i))
+for i in range(2,6):
+    print("Pętla 2 {}".format(i))
+for i in range(14, 6, -2):
+    print("Pętla 3 {}".format(i))
+
+napis = "Ala ma kota"
+for litera in napis:
+    print(litera)
+
+lista = [1,4,67756,324234,546]
+
+for i in lista:
+    print(i)
+
+
+
+print("Podaj liczbę zleceń")
+job_count = int(input())
+workload1 = 0
+workload2 = 0
+workload3 = 0
+for job in range(job_count):
+    print("Podaj liczbę godzin pracy dla zlecenia")
+    job_hours = int(input())
+    if job_hours <= 0:
+        print("Błąd: Nieprawidłowa liczba godzin!")
+        break
+    if workload3 < workload1 and workload3 < workload2:
+        workload3 += job_hours
+    elif workload2 < workload1:
+        workload2 += job_hours
+    else:
+        workload1 += job_hours
+print("Najbliży mechanik będzie wolny w ciągu {} dni".format(
+    int((min(workload1, workload2, workload3)+7)  / 8)
+))
+print("Wszyscy mechanicy będą wolni w ciągu {} dni".format(
+    int((max(workload1, workload2, workload3)+7)  / 8)
+))
+
+import sys
+
+#print(sys.argv[1])
+#print(sys.argv[2])
+#print(sys.argv[3])
+
+print("Podaj liczbę zleceń")
+job_count = int(sys.argv[1])
+workload1 = 0
+workload2 = 0
+workload3 = 0
+for job in range(job_count):
+    print("Podaj liczbę godzin pracy dla zlecenia")
+    job_hours = int(input())
+    if job_hours <= 0:
+        print("Błąd: Nieprawidłowa liczba godzin!")
+        break
+    if workload3 < workload1 and workload3 < workload2:
+        workload3 += job_hours
+    elif workload2 < workload1:
+        workload2 += job_hours
+    else:
+        workload1 += job_hours
+print("Najbliży mechanik będzie wolny w ciągu {} dni".format(
+    int((min(workload1, workload2, workload3)+7)  / 8)
+))
+print("Wszyscy mechanicy będą wolni w ciągu {} dni".format(
+    int((max(workload1, workload2, workload3)+7)  / 8)
+))
+'''
