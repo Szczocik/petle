289b409459adb53338004c9f1529b6f787a58cac
popam <popa.mariusz@gmail.com>
2021-08-17 11:18:07 +0200

    suma i srednia

--- main.py
+++ main.py
@@ -38,3 +38,25 @@ elif a % 3 == 0:
     print("Liczba a jest podzielna 3")
 else:
     print("Liczba nie jest podzielna przez 3,5,15")
+
+count = 0
+sum = 0
+print("Podaj liczbę: ")
+last_number = input()
+while not last_number.isnumeric():
+    print("Podaj liczbę: ")
+    last_number = input()
+last_number = int(last_number)
+while last_number:
+    sum += last_number
+    count += 1 # count = count + 1
+    print("Podaj liczbę: ")
+    last_number = input()
+    while not last_number.isnumeric():
+        print("Podaj liczbę: ")
+        last_number = input()
+    last_number = int(last_number)
+if count:
+    print("Suma: ", sum, "średnia: ", sum/count)
+else:
+    print("Suma: ", 0, "średnia: ", 0)
