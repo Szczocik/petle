e5cf353e0edb99ddabc13407f83e4725cce6f896
popam <popa.mariusz@gmail.com>
2021-08-17 11:15:03 +0200

    zapis z if

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ if a % 2 == 0: #True/False
     print("Liczba a jest parzysta")
     print("Ala ma kota")
 print("Liczba a jest całkowita")
+
+c = True
+if c:
+    print("Warunek prawdziwy!")
+
+
+a = 5
+
+if a % 5 == 0 and a % 3 == 0:
+    print("Liczba a jest podzielna 15")
+else:
+    if a % 5 == 0:
+        print("Liczba a jest podzielna 5")
+    if a % 3 == 0:
+        print("Liczba a jest podzielna 3")
