c3b0ddb96e41d8d213eb98fd74610fcd287f5282
popam <popa.mariusz@gmail.com>
2021-08-17 11:16:25 +0200

    podzielnosc liczby

--- main.py
+++ main.py
@@ -26,3 +26,15 @@ else:
         print("Liczba a jest podzielna 5")
     if a % 3 == 0:
         print("Liczba a jest podzielna 3")
+
+
+a = 4
+
+if (a % 5 == 0 and a % 3 == 0) or a % 2 == 0:
+    print("Liczba a jest podzielna 15 albo parzysta")
+elif a % 5 == 0:
+    print("Liczba a jest podzielna 5")
+elif a % 3 == 0:
+    print("Liczba a jest podzielna 3")
+else:
+    print("Liczba nie jest podzielna przez 3,5,15")
