f385a5eaec94fd99416e925264b55abaa3b94bd8
popam <popa.mariusz@gmail.com>
2021-08-17 11:13:19 +0200

    instrukcje wewnatrz warunku

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+#print("Hello world!")
 
+'''
+if <warunek>:
+    <instrukcje wewnatrz warunku>
+'''
+'''
+a = 3
+
+if a % 2 == 0: #True/False
+    print("Liczba a jest parzysta")
+    print("Ala ma kota")
+print("Liczba a jest całkowita")
