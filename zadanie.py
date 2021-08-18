5dd89b87ec9a28ad5f71c41fe1226546321aa6bd
popam <popa.mariusz@gmail.com>
2021-08-18 16:07:17 +0200

    dopisanie element

--- zadanie.py
+++ zadanie.py
@@ -4,4 +4,6 @@ suma_pustych_kilogramow = 0
 paczka_puste_kilogramy = 0
 while paczka:
     print("podaj elementw kilogramach: ")
-
+ element = int(input())
+    if element == 0 and element < 1 and element > 10:
+        break
