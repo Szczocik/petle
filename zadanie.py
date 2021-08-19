fe4e1146b3c2f314fc347a2a0e32673bd460fe56
popam <popa.mariusz@gmail.com>
2021-08-19 20:34:51 +0200

    robocza wersja kodu zadania

--- zadanie.py
+++ zadanie.py
@@ -1,13 +1,46 @@
-liczba_paczek_wyslanych = 0
+import sys
+liczba_elementow = int(sys.argv[1])
+
 liczba_kilogramow_wyslanych = 0
-suma_pustych_kilogramow = liczba_paczek_wyslanych * 20 - liczba_kilogramow_wyslanych
+
 puste_kilogramy = 0
-paczka = 0
+paczka_kilogramy = 0
+paczka_numer = 0
 suma_elementow = 0
-while paczka < 20:
+najlzejsza_paczka_numer = 0
+najlzejsza_paczka_waga = 20
+
+for numer_elementu in range(liczba_elementow):
     print("podaj element w kilogramach: ")
-    element = int(input())
-    if element == 0 or element < 1 or element > 10:
+    waga_elementu = int(input())
+    #if numer_elementu == 1:
+     #   paczka_numer = 1
+    if waga_elementu == 0:
+        break
+    elif waga_elementu < 1 or waga_elementu > 10:
         print('zła wartość w kg ')
         break
+    else:
+        liczba_kilogramow_wyslanych += waga_elementu
+        if paczka_kilogramy + waga_elementu <= 20:
+            paczka_kilogramy += waga_elementu
+        else:
+            paczka_numer += 1
+            if paczka_kilogramy < najlzejsza_paczka_waga:
+                najlzejsza_paczka_waga = paczka_kilogramy
+                najlzejsza_paczka_numer = paczka_numer
+            paczka_kilogramy = waga_elementu
+paczka_numer += 1
+if paczka_kilogramy < najlzejsza_paczka_waga:
+    najlzejsza_paczka_waga = paczka_kilogramy
+    najlzejsza_paczka_numer = paczka_numer
+
+suma_pustych_kilogramow = paczka_numer * 20 - liczba_kilogramow_wyslanych
+print("\n")
+print(suma_pustych_kilogramow)
+print(najlzejsza_paczka_numer, najlzejsza_paczka_waga)
+print(paczka_numer)
+print(liczba_kilogramow_wyslanych)
+
+
 
