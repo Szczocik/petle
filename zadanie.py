75a492e5023c26ca93742fa107d5bba026d8bc06
popam <popa.mariusz@gmail.com>
2021-08-19 17:00:00 +0200

    nowa linijka suma_elementow

--- zadanie.py
+++ zadanie.py
@@ -3,6 +3,7 @@ liczba_kilogramow_wyslanych = 0
 suma_pustych_kilogramow = liczba_paczek_wyslanych * 20 - liczba_kilogramow_wyslanych
 puste_kilogramy = 0
 paczka = 0
+suma_elementow = 0
 while paczka < 20:
     print("podaj element w kilogramach: ")
     element = int(input())
