f7ffba87b1b3c1f0b972cf43d421ecfb3549b1bd
popam <popa.mariusz@gmail.com>
2021-08-23 12:23:41 +0200

    wersja robocza

--- zadanie1.py
+++ zadanie1.py
@@ -1,23 +1,22 @@
 # ciąg Collatza
 
-print("Podaj liczbę x w przedziale od 1 do 100: ")
+
 ciag_collatza = []
 x = (int(input()))
+wyraz_ciagu = 0
 
+print("Podaj liczbę x w przedziale od 1 do 100: ")
 while x >= 1 and x < 100:
     if x % 2 == 0:
-        x_n = (x / 2)
+        wyraz_ciagu = (x / 2)
         #print(number)
-    ciag_collatza += x_n
-
+    ciag_collatza += wyraz_ciagu
     elif x % 2 == 1:
-        x_n = (3 * x + 1)
+        wyraz_ciagu = (3 * x + 1)
         #print(number)
-    ciag_collatza += x_n
-    continue
+    ciag_collatza += wyraz_ciagu
 
-    elif x_n = 1
-    break
+    continue
 
 print(ciag_collatza)
 
