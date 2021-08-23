755909d993da44f8852eac319f14e60fdaba1803
popam <popa.mariusz@gmail.com>
2021-08-23 11:36:34 +0200

    nowy zapis kodu

--- zadanie1.py
+++ zadanie1.py
@@ -1,2 +1,26 @@
 # ciąg Collatza
-a = x
+#collatz
+
+print("Podaj liczbę x w przedziale 0d 1 do 100:")
+
+x = (int(input()))
+
+def collatz(x):
+    while x != 1:
+
+        if x % 2==0:
+            x = (//2)
+            #print(number)
+            return (print(int()))
+
+        elif x % 2==1:
+             = (3*+1)
+            #print(number)
+            return (print(int()))
+
+        continue
+
+
+collatz(number)
+
+
